a40bd4ac047cab518f15803c051bc16a7bf5920b
Snow <b.mogul02@gmail.com>
2024-03-11 12:33:25 -0400

    Added lsp and formatting

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,55 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"html",
+					"clangd",
+					"cssls",
+					"dockerls",
+					"graphql",
+					"jsonls",
+					"jdtls",
+					"marksmen",
+					"prismals",
+					"pyright",
+					"rust_analyzer",
+					"tailwindcss",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.html.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.graphql.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.marksmen.setup({})
+			lspconfig.prismals.setup({})
+			lspconfig.pyright.setup({})
+			--lspconfig.sqls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.tailwindcss.setup({})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
---
+++ lua/vim-opts.lua
@@ -0,0 +1,14 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("colorscheme habamax")
+
+--vim.cmd("highlight Normal guibg=none")
+--vim.cmd("highlight NonText guibg=none")
+--vim.cmd("highlight Normal ctermbg=none")
+--vim.cmd("highlight NonText ctermbg=none")
