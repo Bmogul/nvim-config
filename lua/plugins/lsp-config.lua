d94f7eb85e9ae124bbdd51049b4b56d2e3006381
Snow <b.mogul02@gmail.com>
2024-06-29 16:50:47 -0400

    GOO

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,7 @@ return {
 					"prismals",
 					"pyright",
 					"rust_analyzer",
+					"gopls",
 				},
 			})
 		end,
@@ -42,6 +43,7 @@ return {
 			lspconfig.prismals.setup({})
 			lspconfig.pyright.setup({})
 			lspconfig.rust_analyzer.setup({})
+			lspconfig.gopls.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
