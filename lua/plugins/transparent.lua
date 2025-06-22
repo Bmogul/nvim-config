9fe623e7b6dcd66fe1d397f9e8ed0c2e9e5a4d41
b.mogul02@gmail.com <b.mogul02@gmail.com>
2025-06-21 23:43:22 -0400

    added back transparent

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,26 @@
+
+-- Assuming you have already set up your plugin manager and lazy loading mechanism
+-- ...
+
+-- Define a function to load the transparent plugin configuration
+local function setup_transparent()
+    require("transparent").setup({
+        groups = {
+            'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+            'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+            'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+            'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+            'EndOfBuffer',
+        },
+        extra_groups = {}, -- Additional groups that should be cleared
+        exclude_groups = {}, -- Groups you don't want to clear
+    })
+end
+
+-- Lazy load the transparent plugin
+return {
+    "xiyaowong/transparent.nvim", -- Replace with the actual path to the transparent plugin
+    config = function()
+        setup_transparent()
+    end,
+}
